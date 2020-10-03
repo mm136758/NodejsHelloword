a95f1b6851269fee82377196207161c3eefc8256
mirika <mm136758@gmail.com>
2020-10-03 14:48:47 +0900

    vu_demo

--- NodejsHelloword/server.js
+++ NodejsHelloword/server.js
@@ -2,21 +2,33 @@
 var http = require('http');
 var port = process.env.PORT || 1337;
 var url = require('url');
+var test = require("./test_moudle");
+var fs = require("fs");
+
+
 const { route } = require('./router');
 
 function start(route) {
     function onRequest(req, res) {
+        
         var pathname = url.parse(req.url).pathname;
         console.log("request for " + pathname + "received");
+        var q = url.parse(req.url, true).query;
+        var txt = q.year + " " + q.month;
         route(pathname);
         res.writeHead(200, { "Content-Type": "text/pain" });
-        res.write("hello word");
-        res.end();
+        res.write(txt);
+        fs.readFile("Page1.html", function (err, data) {
+            res.writeHead(200, { 'Content-Type': 'text/html' });
+           
+            
+        });
     }
     
 
     http.createServer(onRequest).listen(8888);
-        console.log("Server has started.");
+    console.log("Server has started.");
+    
 
     
 }
